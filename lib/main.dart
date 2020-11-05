61d3c78f59dd6c3a931c6d308725fb2d30917b96
Your Name <darwinmuthu450@gmail.com>
2020-11-05 10:06:58 +0530

    sound function is added

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,68 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  final player = AudioCache();
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-                onPressed: () {
-                  player.play('note.wav');
-                },
-                child: Text('click me')),
+          child: Column(
+            children: [
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: Text('click me')),
+              ),
+              Center(
+                child: FlatButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: Text('click me')),
+              ),
+            ],
           ),
         ),
       ),
