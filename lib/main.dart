c3ce6c18705683f84d159ceb7d370d626bf4001b
Your Name <darwinmuthu450@gmail.com>
2020-11-05 10:46:51 +0530

    placed

--- lib/main.dart
+++ lib/main.dart
@@ -13,72 +13,65 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            children: [
-              Center(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: null,
-                ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(150.0),
+              child: Column(
+                children: [
+                  FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.lightGreen,
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: null,
+                  ),
+                  FlatButton(
+                    color: Colors.pink,
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: null,
+                  ),
+                ],
               ),
-              Center(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: null,
-                ),
-              ),
-              Center(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: null,
-                ),
-              ),
-              Center(
-                child: FlatButton(
-                  color: Colors.lightGreen,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: null,
-                ),
-              ),
-              Center(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: null,
-                ),
-              ),
-              Center(
-                  child: FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                },
-                child: null,
-              )),
-              Center(
-                child: FlatButton(
-                  color: Colors.pink,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: null,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
